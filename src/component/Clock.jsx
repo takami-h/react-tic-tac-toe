60d5798877ae0d6b3c8d39a2166fe98499dcae9b
Takami Hirata <takami-h@natswell.com>
2020-07-10 18:24:19 +0900

    feat: introduce useReducer

--- src/component/Clock.jsx
+++ src/component/Clock.jsx
@@ -1,23 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 function FormattedDate(props) {
   return (<span>{props.date.toLocaleTimeString()}</span>);
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'tick':
+      return new Date();
+    default:
+      throw new Error();
+  }
 }
 
 /*
  * custom hook.
  */
 function usePeriodicDate(interval) {
-  // state for function component.
-  const [date, setDate] = useState(new Date());
+  // useReducer
+  // Actionをディスパッチするだけ、ロジックがコンポーネントの外に出る。
+  const [date, dispatch] = useReducer(reducer, new Date());
+  const tick = () => dispatch({type: 'tick'});
+
+  // useState
+  // 関数コンポーネントでstateを使う方法。
+  // const [date, setDate] = useState(new Date());
+  // const tick = () => setDate(new Date());
 
   // event listener for function component.
   useEffect(() => {
     const intervalMillis = interval || 1000;
-    const timerID = setInterval(() => setDate(new Date()), intervalMillis);
+    const timerID = setInterval(tick, intervalMillis);
+    console.debug(`set timer of id ${timerID} with interval ${intervalMillis} ms.`);
 
     return () => {
       clearInterval(timerID);
+      console.debug(`clear timer of id ${timerID}`);
     };
   }, [interval]); // setInterval only once.
   return date;

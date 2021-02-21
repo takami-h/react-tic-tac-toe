78678ce829c07e316ae4208bb8c2317c965d3dda
Takami Hirata <cmnd.opt.esc@gmail.com>
2021-02-21 15:58:54 +0900

    test: using react testing library

--- src/component/Clock.test.js
+++ src/component/Clock.test.js
@@ -1,36 +1,20 @@
 import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import { render, screen } from '@testing-library/react';
 
 import { Clock } from "./Clock";
 
-let container = null;
-beforeEach(() => {
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
 describe("Clock component", () => {
   it("shows time", () => {
-    act(() => {
-      render(<Clock interval={1000} />, container);
-    });
-    expect(container.textContent).toMatch(/It is .*\./);
-    const dateRendered = new Date(
-      container.querySelector("time").getAttribute("datetime")
-    );
+    const { container } = render(<Clock interval={1000} />);
+
+    const content = screen.getByText(/It is .*\./);
+    expect(content).not.toBeNull();
+    const dateRendered = new Date(container.getElementsByTagName('time')[0].getAttribute('datetime'));
     const now = new Date();
-    expect(now.getTime() - dateRendered.getTime()).toBeLessThan(1000);
+    expect(now.getTime() - dateRendered).toBeLessThan(1000);
   });
   it("should match snapshot", () => {
-    act(() => {
-      render(<Clock interval={1000} />, container);
-    });
+    render(<Clock interval={1000} />);
     // expect(container.innerHTML).toMatchInlineSnapshot();
   });
 });
